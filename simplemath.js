bcc39250af89d5fd376f491432fb45db953ee5bb
Anant Pathak <141476557+Anant-Pathak9@users.noreply.github.com>
2025-08-13 17:04:08 +0545

    Add files via upload

---
+++ simplemath.js
@@ -0,0 +1,16 @@
+function minusSeven(num) {
+    return num-7;
+}
+function mult(num) {
+    return num*7;
+}
+function add(num) {
+    return num+7;
+}
+function div(num) {
+    return num/7;
+}
+console.log(minusSeven(10));
+console.log(mult(10));
+console.log(add(10));
+console.log(div(10));
